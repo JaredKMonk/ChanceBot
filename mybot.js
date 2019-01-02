7dba65ed4788b3ad0da16dcb62d511df148f3d88
Jared Monk <jmonk@jaredmonkdigital.com>
2019-01-01 21:07:39 -0600

    initial commit; rudimentary functionality

---
+++ mybot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const Chance = require("chance");
+const chance = new Chance();
+
+client.on("ready", () => {
+    console.log("I am ready!");
+});
+
+var testArray = ["Mahvel","Skullgirls","SoulCal6","Tekken7","SFV","Third Strike","Second Impact","Smash","Project M"];
+
+client.on("message", (message) => {
+    if (message.content.startsWith("ping")){
+        message.channel.send("pong!");
+    }
+    if (message.content.startsWith("rando")){
+        var chosen = [];
+        var i = 0;
+        do {
+            var option = chance.pickone(testArray);
+            if (chosen.includes(option)){ continue } else{
+            chosen[i] = option;
+            i++;}
+        } while (chosen.length < 3);
+        message.channel.send(chosen[0] + "," + chosen[1] + "," + chosen[2]);
+    }
+});
+
+client.login("NTI5Nzk3NzU3ODE3NTg1NjY2.Dw2GCg.bN5Xo9qYpcxIO5ixX1phVvtp7YU");
